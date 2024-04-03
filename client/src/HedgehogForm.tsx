7a32f5f44bfd82755d089147c6b3ee340b731ae6
Anakino <ankku.ojala@gmail.com>
2024-04-03 17:59:09 +0300

    Create base form for adding hedgehog

--- client/src/HedgehogForm.tsx
+++ client/src/HedgehogForm.tsx
@@ -1,4 +1,6 @@
-import { Paper, Typography } from "@mui/material";
+import {
+  Button, Box, FormControl, FormControlLabel, FormLabel, Paper, Radio, RadioGroup, TextField, Typography
+} from "@mui/material";
 
 interface Props {
   coordinates: number[];
@@ -15,12 +17,43 @@ export function HedgehogForm({ coordinates }: Props) {
         padding: "1em",
       }}
     >
-      <Typography>
-        TODO: Luo tähän lomake painikkeineen, jonka avulla uusia siilihavaintoja
-        saa lisättyä palveluun.
+    <Box
+      sx={{
+        backgroundColor: "#a1e6df",
+        height: "3em",
+        display: "flex",
+        zIndex: 2,
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Typography sx={{fontSize: 18}}>
+        Lisää uusi siilihavainto
       </Typography>
-      <br />
-      <Typography>
+    </Box>
+    <FormControl sx={{
+        padding: "1em",
+        display: "flex",
+        justifyContent: "center"
+      }}>
+        <FormLabel required>Nimi</FormLabel>
+        <TextField></TextField>
+        <FormLabel required>Ikä</FormLabel>
+        <TextField type="number"></TextField>
+        <FormLabel id="gender-radio-buttons-group-label">Sukupuoli</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="gender-radio-buttons-group-label"
+          defaultValue="female"
+          name="gender-radio-buttons-group"
+        >
+          <FormControlLabel value="female" control={<Radio />} label="Female" />
+          <FormControlLabel value="male" control={<Radio />} label="Male" />
+          <FormControlLabel value="other" control={<Radio />} label="Other" />
+        </RadioGroup>
+        <Button variant="contained">Submit</Button>
+    </FormControl>
+      <Typography sx={{ padding: "1em" }}>
         Siililtä kysyttävät tiedot: nimi, ikä, sukupuoli. Lisäksi siilin
         havainnon yhteydessä merkitään havainnon sijainti kartalla. Kartalta
         saadaan koordinaattipiste tälle HedgehogForm:lle klikkaamalla karttaa
